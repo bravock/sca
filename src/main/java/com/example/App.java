0d75bb827c3af96a5ab5c97b071b56eeca29d3e0
Jhonatan de Jesus Sousa <jsousa@alelo.com.br>
2025-03-20 15:17:54 -0300

    🚀 Primeiro commit - Adicionando projeto Maven vulnerável

---
+++ src/main/java/com/example/App.java
@@ -0,0 +1,7 @@
+package com.example;
+
+public class App {
+    public static void main(String[] args) {
+        System.out.println("🚀 Aplicação Maven com dependência vulnerável não utilizada!");
+    }
+}
